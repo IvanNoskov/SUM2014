92d552e654fd59f851ffbb6b38c375bcbf554556
unknown <SPR01@R111-21.(none)>
2014-06-03 18:21:34 +0400

    T02CLOCK V1.0
    fully worced

--- T02Clock/T02clock.C
+++ T02Clock/T02clock.C
@@ -6,19 +6,19 @@
 #include <windows.h>
 
 #define WND_CLASS_NAME "my_fist's_window_class"
-#define PI (3,1415926535897)
+#define PI ((double)(3.1415926535897))
 
-void DrawHour (HDC hDCP, double A, int X, int Y)
+void DrawSec (HDC hDCP, double A, int X, int Y)
 {
   static POINT T[4];
-  T[0].x = X + 50 * cos( A );
-  T[0].y = Y - 50 * sin( A );
-  T[1].x = X + 40 * cos( A + 1 );
-  T[1].y = Y - 40 * sin( A + 1 );
-  T[2].x = X + 70 * cos( A + PI );
-  T[2].y = Y - 70 * sin( A + PI );
-  T[3].x = X + 40 * cos( A - 1 );
-  T[3].y = Y - 40 * sin( A - 1 );
+  T[0].x = X - 50 * sin( A );
+  T[0].y = Y + 50 * cos( A );
+  T[1].x = X - 40 * sin( A + 1 );
+  T[1].y = Y + 40 * cos( A + 1 );
+  T[2].x = X - 70 * sin( A + PI );
+  T[2].y = Y + 70 * cos( A + PI );
+  T[3].x = X - 40 * sin( A - 1 );
+  T[3].y = Y + 40 * cos( A - 1 );
 
   SelectObject( hDCP, GetStockObject( DC_PEN ) );
   SelectObject( hDCP, GetStockObject( DC_BRUSH ) );
@@ -27,6 +27,47 @@ void DrawHour (HDC hDCP, double A, int X, int Y)
 
   Polygon( hDCP, T, 4 );
 }
+
+void DrawMin (HDC hDCP, double A, int X, int Y)
+{
+  static POINT T[4];
+  T[0].x = X - 70 * sin( A );
+  T[0].y = Y + 70 * cos( A );
+  T[1].x = X - 60 * sin( A + 1 );
+  T[1].y = Y + 60 * cos( A + 1 );
+  T[2].x = X - 90 * sin( A + PI );
+  T[2].y = Y + 90 * cos( A + PI );
+  T[3].x = X - 60 * sin( A - 1 );
+  T[3].y = Y + 60 * cos( A - 1 );
+
+  SelectObject( hDCP, GetStockObject( DC_PEN ) );
+  SelectObject( hDCP, GetStockObject( DC_BRUSH ) );
+  SetDCPenColor( hDCP, RGB( 0, 0, 0 ) );
+  SetDCBrushColor( hDCP, RGB( 100, 255, 100 ) );
+
+  Polygon( hDCP, T, 4 );
+}
+
+void DrawHour (HDC hDCP, double A, int X, int Y)
+{
+  static POINT T[4];
+  T[0].x = X - 90 * sin( A );
+  T[0].y = Y + 90 * cos( A );
+  T[1].x = X - 80 * sin( A + 1 );
+  T[1].y = Y + 80 * cos( A + 1 );
+  T[2].x = X - 110 * sin( A + PI );
+  T[2].y = Y + 110 * cos( A + PI );
+  T[3].x = X - 80 * sin( A - 1 );
+  T[3].y = Y + 80 * cos( A - 1 );
+
+  SelectObject( hDCP, GetStockObject( DC_PEN ) );
+  SelectObject( hDCP, GetStockObject( DC_BRUSH ) );
+  SetDCPenColor( hDCP, RGB( 0, 0, 0 ) );
+  SetDCBrushColor( hDCP, RGB( 100, 100, 255 ) );
+
+  Polygon( hDCP, T, 4 );
+}
+
 
 
 LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam )
@@ -37,16 +78,15 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
   static INT W, H;
   static HDC hMemDC, hDCLogo;
   static HBITMAP hBm, hBmLogo;
+  static BITMAP BmLogo;
   SYSTEMTIME Time;
   static double Hour, Min, Sec;
-  static POINT Trangle[3] = {{10, 10}, {100, 15}, {15, 100}};
   switch(Msg)
   {
   case WM_CREATE:
-    hDC = GetDC( hWnd );
-    hMemDC = CreateCompatibleBitmap( hDC, W, H);
-    ReleaseDC ( hWnd, hDC );
     SetTimer( hWnd, 30, 100, NULL );
+    hBmLogo = LoadImage( NULL, "clockface.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    GetObject(hBmLogo, sizeof(BITMAP), &BmLogo);
     return 0;
   case WM_SIZE:
     H = HIWORD(lParam);
@@ -58,27 +98,36 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
     ReleaseDC( hWnd, hDC );
     SelectObject( hMemDC, hDC );
   case WM_TIMER:
-    GetSystemTime( &Time );
-    Sec = 60.0 * 2 * PI / Time.wSecond;
-    Min = 60 * 60.0 * 2 * PI / (60 * Time.wMinute + Time.wSecond);
-    Hour = 12 * 60 * 60.0 * 2 * PI / (120 * (Time.wHour % 12) + 60 * Time.wMinute + Time.wSecond);
+    GetLocalTime( &Time );
+    Sec = (double)2 * PI * Time.wSecond / 60;
+    Min = (double)2 * PI * (Time.wMinute * 60 + Time.wSecond) / (60 * 60);
+    Hour = (double)2 * PI * ((Time.wHour % 12) * 60 * 60 + Time.wMinute * 60 + Time.wSecond) / (12 * 60 * 60);
     InvalidateRect( hWnd, NULL, FALSE);
     break;
   case WM_DESTROY:
     KillTimer(hWnd, 30);
     DeleteObject( hBm );
-    DeleteDC( hMemDC );
+    DeleteObject ( hBmLogo );
     PostQuitMessage(0);
     return 0;
   case WM_PAINT:
     hDC = BeginPaint( hWnd, &ps );
-    SelectObject( hDC, GetStockObject( DC_PEN ) );
-    SelectObject( hDC, GetStockObject( DC_BRUSH ) );
-    SetDCPenColor( hDC, RGB( 0, 0, 0 ) );
-    SetDCBrushColor( hDC, RGB( 255, 0, 255 ) );
+    hMemDC = CreateCompatibleDC( hDC );
+    hDCLogo = CreateCompatibleDC( hDC );
+    SelectObject( hMemDC, hBm );
+    SelectObject( hDCLogo, hBmLogo );
+    SelectObject( hMemDC, GetStockObject( DC_PEN ) );
+    SelectObject( hMemDC, GetStockObject( DC_BRUSH ) );
+    SetDCPenColor( hMemDC, RGB( 0, 0, 0 ) );
+    SetDCBrushColor( hMemDC, RGB( 255, 255, 255 ) );
     Rectangle( hMemDC, 0, 0, W, H);
+    BitBlt( hMemDC, (W - BmLogo.bmWidth) / 2, (H - BmLogo.bmHeight) / 2, BmLogo.bmWidth, BmLogo.bmHeight, hDCLogo, 0, 0, SRCCOPY);
     DrawHour(hMemDC, Hour, W / 2, H / 2);
-    BitBlt(hDC, 0, 0, W, H, hMemDC, 0, 0, SRCCOPY);
+    DrawMin(hMemDC, Min, W / 2, H / 2);
+    DrawSec(hMemDC, Sec, W / 2, H / 2);
+    BitBlt( hDC, 0, 0, W, H, hMemDC, 0, 0, SRCCOPY);
+    DeleteDC( hMemDC );
+    DeleteDC( hDCLogo );
     EndPaint( hWnd, &ps );
     break;
   }
